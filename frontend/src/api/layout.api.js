be993e4704b28fff443d6688f26d3a587496b232
Samisangi <Samiullah.bscsf22@iba-suk.edu.pk>
2026-07-10 21:58:04 +0500

    add jspdf

--- frontend/src/api/layout.api.js
+++ frontend/src/api/layout.api.js
@@ -2,4 +2,5 @@ import axiosInstance from "./axiosInstance";
 
 export const generateLayouts = (projectId) =>
   axiosInstance.post(`/layouts/${projectId}/generate`);
-export const getLayouts = (projectId) => axiosInstance.get(`/layouts/${projectId}`);
+export const getLayouts = (projectId) => axiosInstance.get(`/layouts/${projectId}`);export const saveLayoutEdits = (projectId, layoutId, rooms) =>
+  axiosInstance.put(`/layouts/${projectId}/${layoutId}`, { rooms });
